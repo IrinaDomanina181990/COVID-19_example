cfa7977be715adbe6bd3b58f04030820563d65f8
IrinaDomanina181990 <barankova.i1990@yandex.ru>
2026-06-04 21:05:09 +0400

    count male female

---
+++ COVID-19_Domanina 04 06 select.R
@@ -0,0 +1,8 @@
+mean(Covid.Data[["x3"]]
+max(Covid.DATA[["INTUBED"]])
+max(Covid.Data[["INTUBED"]])     
+min(Covid.Data[["INTUBED"]])
+Select(Covid.Data, SEX)
+library(dplyr)
+select(Covid.Data, SEX)
+count(Covid.Data, SEX)
