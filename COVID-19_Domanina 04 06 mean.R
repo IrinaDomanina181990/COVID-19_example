0f8be918f4092bfb8b35b1b9974545277d80c7ec
IrinaDomanina181990 <barankova.i1990@yandex.ru>
2026-06-04 20:40:40 +0400

    select collumn

--- COVID-19_Domanina 04 06 mean.R
+++ COVID-19_Domanina 04 06 mean.R
@@ -1,3 +1,7 @@
 mean(Covid.Data[["x3"]]
 max(Covid.DATA[["INTUBED"]])
 max(Covid.Data[["INTUBED"]])     
+min(Covid.Data[["INTUBED"]])
+Select(Covid.Data, SEX)
+library(dplyr)
+select(Covid.Data, SEX)
